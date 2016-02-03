8ac3961f429528d1ef7886688fcbf341efc264db
Wong Sui On <sowong411@gmail.com>
2016-02-04 00:48:51 +0800

    1.Apply the gradient on the Action Bar 2.Change App Name

--- app/src/main/java/com/example/onzzz/kcdsc/MainActivity.java
+++ app/src/main/java/com/example/onzzz/kcdsc/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.onzzz.kcdsc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.ActionBar;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActionBar actionBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Get the template Action Bar as our Action Bar
+        actionBar = getSupportActionBar();
+
+        //Set the return arrow in the Action Bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        //Apply the gradient(漸變色) in the Action Bar
+        actionBar.setBackgroundDrawable(getResources().getDrawable(R.drawable.actionbar_gradient_bg));
     }
 }
